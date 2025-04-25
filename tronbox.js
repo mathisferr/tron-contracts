82ace4ec9d5ee372ef2201a80ba57914f7cfbfba
MATHIS-DEKSTOP <math.ferrucci@gmail.com>
2025-04-25 14:05:37 +0200

    Add launch configuration for debugging in VSCode

--- tronbox.js
+++ tronbox.js
@@ -1,8 +1,8 @@
 module.exports = {
     networks: {
         development: {
-            from: '',
-            privateKey: '',
+            from: 'TNQ4GvRrBkxJkb4xH3YEMeyFGsdwGzMutP',
+            privateKey: 'b6592b66e4ddff77be1d6a1633db3fc0a226e052416d7c198f5ae0af079adcd1',
             userFeePercentage: 30,
             feeLimit: 1e9,
             originEnergyLimit: 1e7,
